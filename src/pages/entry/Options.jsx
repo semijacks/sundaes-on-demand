a1f656e8bb4033b558ee02478b607ea5469619ec
Semilore Idowu <semijacks27@gmail.com>
2021-12-09 13:51:10 +0100

    Test and code context for scoops subtotals

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -2,29 +2,49 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import ScoopOption from './ScoopOption';
+import ToppingOption from './ToppingOption';
+import AlertBanner from '../common/AlertBanner';
+import { pricePerItem } from '../../constants';
+import { useOrderDetails } from '../../contexts/OrderDetails';
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
+  const [orderDetails, updateItemCount] = useOrderDetails();
   // optionType is either 'scoops' or 'toppings'
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((err) => {
-        // TODO: handle error response
-      });
+      .catch((err) => setError(true));
   }, [optionType]);
 
-  // TODO: replace null with topping option when available
-  const ItemComponent = optionType === 'scoops' ? ScoopOption : null;
+  if (error) {
+    return <AlertBanner />;
+  }
+
+  const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
+  const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
 
   const optionItems = items.map((item) => (
     <ItemComponent
       key={item.name}
       name={item.name}
       imagePath={item.imagePath}
+      updateItemCount={(itemName, newItemCount) =>
+        updateItemCount(itemName, newItemCount, optionType)
+      }
     />
   ));
 
-  return <Row>{optionItems}</Row>;
+  return (
+    <>
+      <h2>{title}</h2>
+      <p>{pricePerItem[optionType]} each</p>
+      <p>
+        {title} total: {orderDetails.totals[optionType]}
+      </p>
+      <Row>{optionItems}</Row>
+    </>
+  );
 }
--- src/pages/entry/test/Options.test.jsx
+++ src/pages/entry/test/Options.test.jsx
@@ -1,15 +1,33 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '../../../test-utils/testing-library';
 
 import Options from '../Options';
 
 test('displays image for each scoop option from the server', async () => {
   render(<Options optionType='scoops' />);
 
-  // find images
+  // find scoops images
   const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
   expect(scoopImages).toHaveLength(2);
 
-  // confirm alt text of images
+  // confirm alt text of scoops images
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
+});
+
+test('displays image for each topping option from the server', async () => {
+  render(<Options optionType='toppings' />);
+
+  //find toppings images
+  const toppingsImages = await screen.findAllByRole('img', {
+    name: /topping$/i,
+  });
+  expect(toppingsImages).toHaveLength(3);
+
+  // confirm alt text of toppings images
+  const altText = toppingsImages.map((element) => element.alt);
+  expect(altText).toStrictEqual([
+    'M&Ms topping',
+    'Hot fudge topping',
+    'Cherries topping',
+  ]);
 });
