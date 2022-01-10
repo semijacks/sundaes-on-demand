413f7f9c413fb64eadcfc957375b23246ecbabaf
Semilore Idowu <semijacks27@gmail.com>
2022-01-10 13:01:47 +0100

    added new happy path test

--- src/test/orderPhase.test.jsx
+++ src/test/orderPhase.test.jsx
@@ -66,11 +66,19 @@ test('check order phases for happy path', async () => {
   });
   userEvent.click(confirmOrderButton);
 
+  // check "Loading" text appears
+  const loadingText = screen.getByText(/loading/i);
+  expect(loadingText).toBeInTheDocument();
+
   // confirm order number on confirmation page
   const thankYouHeader = await screen.findByRole('heading', {
     name: /thank you/i,
   });
   expect(thankYouHeader).toBeInTheDocument();
+
+  // check "Loading" text disappears
+  const loading = screen.queryByText(/loading/i);
+  expect(loading).not.toBeInTheDocument();
 
   const orderNumber = await screen.findByText(/order number/i);
   expect(orderNumber).toBeInTheDocument();
@@ -89,4 +97,41 @@ test('check order phases for happy path', async () => {
   // do we need to need to await anything to avoid test errors
   await screen.findByRole('spinbutton', { name: 'Vanilla' });
   await screen.findByRole('checkbox', { name: /hot fudge/i });
+});
+
+test('toppings header is not on summary page if no toppings is ordered', async () => {
+  // render app
+  render(<App />);
+
+  // add ice cream scoops and toppings
+  const vanillaInput = await screen.findByRole('spinbutton', {
+    name: /vanilla/i,
+  });
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, '1');
+
+  const chocolateInput = await screen.findByRole('spinbutton', {
+    name: /chocolate/i,
+  });
+
+  userEvent.clear(chocolateInput);
+  userEvent.type(chocolateInput, '2');
+
+  // find and click order summary button
+  const orderSummaryButton = screen.getByRole('button', {
+    name: /order sundae/i,
+  });
+  userEvent.click(orderSummaryButton);
+
+  // check summary information based on order
+  const summaryHeading = screen.getByRole('heading', {
+    name: /order summary/i,
+  });
+  expect(summaryHeading).toBeInTheDocument();
+
+  const scoopsHeading = screen.getByRole('heading', { name: 'Scoops: $6.00' });
+  expect(scoopsHeading).toBeInTheDocument();
+
+  const toppingsHeading = screen.queryByRole('heading', { name: /toppings/i });
+  expect(toppingsHeading).not.toBeInTheDocument();
 });
